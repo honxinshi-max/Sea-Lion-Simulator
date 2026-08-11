08cfd247619738e1ae3f25a36a6701977b0467c0
Dr.X <honxinshi@gmail.com>
2026-08-11 20:45:49 +0800

    Update creator name in birthday configuration

--- src/game/config/birthday.ts
+++ src/game/config/birthday.ts
@@ -2,6 +2,6 @@ export const BIRTHDAY_CONFIG = {
   enabled: true,
   addressee: '爸爸',
   date: '2026-08-24',
-  creator: '制作者署名（请在 src/game/config/birthday.ts 修改）',
+  creator: '大鹏（请在 src/game/config/birthday.ts 修改）',
   message: '爸爸，生日快乐！\n谢谢你教会我，不只是寻找答案，还要发现新的方法。',
 } as const;
